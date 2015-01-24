c1cdd5d0d4463793beb1d87fd2491c02800ae3f3
Emanuele Minotto <minottoemanuele@gmail.com>
2015-01-24 07:15:41 +0100

    configuration + optional twig var

--- DependencyInjection/FakerExtension.php
+++ DependencyInjection/FakerExtension.php
@@ -4,13 +4,15 @@ namespace EmanueleMinotto\FakerBundle\DependencyInjection;
 
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Definition;
 use Symfony\Component\DependencyInjection\Loader;
+use Symfony\Component\DependencyInjection\Reference;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 
 /**
  * This is the class that loads and manages your bundle configuration.
  *
- * @link     http://symfony.com/doc/current/cookbook/bundles/extension.html
+ * @link http://symfony.com/doc/current/cookbook/bundles/extension.html
  */
 class FakerExtension extends Extension
 {
@@ -26,10 +28,36 @@ class FakerExtension extends Extension
      */
     public function load(array $configs, ContainerBuilder $container)
     {
+        $configuration = new Configuration();
+        $configs = $this->processConfiguration($configuration, $configs);
+
+        if ($configs['twig']) {
+            $this->loadTwigExtension($container);
+        }
+
         $loader = new Loader\XmlFileLoader(
             $container,
             new FileLocator(__DIR__.'/../Resources/config')
         );
         $loader->load('services.xml');
+    }
+
+    /**
+     * Loads the Twig global variable.
+     * 
+     * @param  ContainerBuilder $container
+     */
+    private function loadTwigExtension(ContainerBuilder $container)
+    {
+        $service = new Definition(
+            'EmanueleMinotto\FakerBundle\Twig\FakerGeneratorExtension',
+            array(
+                new Reference('faker'),
+            )
+        );
+        $service->addTag('twig.extension');
+        $service->setPublic(false);
+
+        $container->setDefinition('emanuele.minotto.faker.twig.extension', $service);
     }
 }
