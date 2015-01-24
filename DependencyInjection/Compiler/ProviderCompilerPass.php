204047225832088f4a5ab3329dffcaea2b98060d
Emanuele Minotto <minottoemanuele@gmail.com>
2015-01-24 04:46:45 +0100

    added tags

---
+++ DependencyInjection/Compiler/ProviderCompilerPass.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace EmanueleMinotto\FakerBundle\DependencyInjection\Compiler;
+
+use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Reference;
+
+/**
+ * Provide the different providers for Faker.
+ *
+ * @link https://github.com/fzaninotto/Faker#faker-internals-understanding-providers
+ */
+class ProviderCompilerPass implements CompilerPassInterface
+{
+    public function process(ContainerBuilder $container)
+    {
+        if (false === $container->hasDefinition('faker')) {
+            return;
+        }
+
+        $definition = $container->getDefinition('faker');
+
+        foreach ($container->findTaggedServiceIds('faker.provider') as $id => $attributes) {
+            $providerDefinition = $container->getDefinition($id);
+            $providerDefinition->setArguments(array(
+                new Reference('faker'),
+            ));
+
+            $definition->addMethodCall('addProvider', array(
+                new Reference($id),
+            ));
+        }
+    }
+}
--- FakerBundle.php
+++ FakerBundle.php
@@ -3,10 +3,20 @@
 namespace EmanueleMinotto\FakerBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
+use EmanueleMinotto\FakerBundle\DependencyInjection\Compiler\ProviderCompilerPass;
 
 /**
  * {@inheritdoc}
  */
 class FakerBundle extends Bundle
 {
+    /**
+     * {@inheritdoc}
+     */
+    public function build(ContainerBuilder $container)
+    {
+        parent::build($container);
+
+        $container->addCompilerPass(new ProviderCompilerPass());
+    }
 }
