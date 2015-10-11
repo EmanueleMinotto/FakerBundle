593ee0c5bb65a8bb1f5417dfd5c9afd1eb1ef450
Travis <travis@travis-ci.org>
2015-10-11 09:46:39 +0000

    API updated

---
+++ elementlist.js
@@ -0,0 +1,3 @@
+
+var ApiGen = ApiGen || {};
+ApiGen.elements = [["c","EmanueleMinotto\\FakerBundle\\DependencyInjection\\Compiler\\ProviderCompilerPass"],["c","EmanueleMinotto\\FakerBundle\\DependencyInjection\\Configuration"],["c","EmanueleMinotto\\FakerBundle\\DependencyInjection\\FakerExtension"],["c","EmanueleMinotto\\FakerBundle\\FakerBundle"],["c","EmanueleMinotto\\FakerBundle\\Twig\\FakerGeneratorExtension"]];
